052839fed0dfa6acf6b7ba68b01457067ff00445
AlexanderLislelid <alexlislelid@gmail.com>
2026-02-11 14:26:17 +0100

    update router

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import { HomeView } from "./views/HomeView.js";
 import { NotfoundView } from "./views/NotFoundView.js";
-import { LoginView, handleLogin } from "./views/LoginView.js";
-import { RegisterView, handleRegister } from "./views/RegisterView.js";
+import { LoginView, initLogin } from "./views/LoginView.js";
+import { RegisterView } from "./views/RegisterView.js";
 import { ProfileView } from "./views/ProfileView.js";
 
 export function router() {
@@ -18,8 +18,8 @@ export function router() {
 
     document.getElementById("app").innerHTML = await view();
 
-    if (hash === "#/login") handleLogin();
-    if (hash === "#/register") handleRegister();
+    if (hash === "#/login") initLogin();
+    // if (hash === "#/register") handleRegister();
   }
 
   window.addEventListener("hashchange", handleRoute);
