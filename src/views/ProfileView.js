c869108b66ce84aeca60ed60140cedf3ab047695
AlexanderLislelid <alexlislelid@gmail.com>
2026-02-11 15:39:16 +0100

    add function to render profile and set a welcome message

--- src/views/ProfileView.js
+++ src/views/ProfileView.js
@@ -1,7 +1,18 @@
+import { loadToken, removeToken } from "../utils/storage.js";
+import { get } from "../api/apiClient.js";
+
 export function ProfileView() {
   return /* HTML */ `
     <section class="flex flex-col items-center">
-      <h1>Welcome back</h1>
+      <h1 id="profileWelcome"></h1>
     </section>
   `;
+}
+
+export async function renderProfile() {
+  const username = localStorage.getItem("username");
+  const profile = await get(`social/profiles/${username}`);
+
+  document.getElementById("profileWelcome").textContent =
+    `Welcome back ${profile.data.name}`;
 }
