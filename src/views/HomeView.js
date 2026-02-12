a97e23adf929f08da93c0e089cc2250dda4a134e
AlexanderLislelid <alexlislelid@gmail.com>
2026-02-12 11:21:50 +0100

    add token and API key validation in HomeView

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,20 +1,25 @@
-export async function HomeView() {
-  return /* HTML */ `
-    <section class="px-4 md:max-w-4xl md:mx-auto">
-      <div
-        class="bg-white rounded-lg shadow p-4 mb-6 flex flex-col md:flex-row gap-4"
-      >
-        <textarea
-          placeholder="What's on your mind?"
-          class="flex-1 border rounded px-3 py-2 h-10 resize-none"
-        ></textarea>
+import { loadToken, loadApiKey } from "../utils/storage.js";
 
-        <button
-          class="shrink-0 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 w-full md:w-auto"
+export async function HomeView() {
+  if (loadToken() && loadApiKey()) {
+    return /* HTML */ `
+      <section class="px-4 md:max-w-4xl md:mx-auto">
+        <div
+          class="bg-white rounded-lg shadow p-4 mb-6 flex flex-col md:flex-row gap-4"
         >
-          Create Post
-        </button>
-      </div>
-    </section>
-  `;
+          <textarea
+            placeholder="What's on your mind?"
+            class="flex-1 border rounded px-3 py-2 h-10 resize-none"
+          ></textarea>
+
+          <button
+            class="shrink-0 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 w-full md:w-auto"
+          >
+            Create Post
+          </button>
+        </div>
+      </section>
+    `;
+  }
+  window.location.hash = "#/login";
 }
