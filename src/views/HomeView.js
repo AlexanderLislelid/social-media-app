62205c41d67246c667bcee26d8fd2904d7db9937
AlexanderLislelid <alexlislelid@gmail.com>
2026-02-12 21:50:04 +0100

    change create post section to search form with submit button

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -6,20 +6,20 @@ export async function HomeView() {
   if (loadToken() && loadApiKey()) {
     return /* HTML */ `
       <section class="px-4 md:max-w-4xl md:mx-auto">
-        <div
+        <form id="search-form"
           class="bg-white rounded-lg shadow p-4 mb-6 flex flex-col md:flex-row gap-4"
         >
-          <textarea
-            placeholder="What's on your mind?"
+          <input type="search"
+            placeholder="Search for posts"
             class="flex-1 border rounded px-3 py-2 h-10 resize-none"
-          ></textarea>
+          ></input>
 
           <button
             class="shrink-0 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 w-full md:w-auto"
           >
-            Create Post
+            Search posts
           </button>
-        </div>
+        </form>
       </section>
       <section id="feed">
         <div id="posts"></div>
