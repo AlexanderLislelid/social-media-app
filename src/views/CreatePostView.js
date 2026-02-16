5fff24597245d6d9374e5c459c3786dafd834c73
AlexanderLislelid <alexlislelid@gmail.com>
2026-02-16 14:33:45 +0100

    add styling to post creation form

--- src/views/CreatePostView.js
+++ src/views/CreatePostView.js
@@ -4,23 +4,50 @@ import { post } from "../api/apiClient.js";
 export async function CreatePostView() {
   if (loadToken() && loadApiKey()) {
     return /* HTML */ `
-      <h1 class="text-center text-2xl font-bold">
-        Create your first socialApp post
-      </h1>
-      <form id="create-form" class="flex flex-col gap-4">
-        <div>
-          <label for="post-title">Title</label>
-          <input type="textarea" id="post-title" />
+      <form
+        id="create-form"
+        class="max-w-md mx-auto bg-slate-800 border border-slate-700 rounded-xl shadow p-6 space-y-6 mt-10"
+      >
+        <h1 class="text-center text-2xl font-bold text-slate-100 mt-4 mb-10">
+          Create your first socialApp post
+        </h1>
+        <div class="flex flex-col">
+          <label for="post-title" class="text-sm text-slate-300 ml-1 mb-1"
+            >Give your post a title</label
+          >
+          <input
+            type="text"
+            id="post-title"
+            class="bg-slate-900 border border-slate-700 text-slate-100 placeholder:text-slate-400 rounded-lg px-3 py-2 text-sm outline-none focus:outline-none focus:border-indigo-500"
+          />
         </div>
-        <div>
-          <label for="post-content">Body</label>
-          <input type="textarea" id="post-content" />
+        <div class="flex flex-col">
+          <label for="post-content" class="text-sm text-slate-300 ml-1 mb-1"
+            >Write your post</label
+          >
+          <textarea
+            id="post-content"
+            rows="6"
+            class="bg-slate-900 border border-slate-700 text-slate-100 placeholder:text-slate-400 rounded-lg px-3 py-2 text-sm outline-none focus:outline-none focus:border-indigo-500"
+          >
+          </textarea>
         </div>
-        <div>
-          <label for="post-media-url">Image URL</label>
-          <input type="textarea" id="post-media-url" />
+        <div class="flex flex-col">
+          <label for="post-media-url" class="text-sm text-slate-300 ml-1 mb-1"
+            >Attatch an image URL</label
+          >
+          <input
+            type="text"
+            id="post-media-url"
+            class="bg-slate-900 border border-slate-700 text-slate-100 placeholder:text-slate-400 rounded-lg px-3 py-2 text-sm outline-none focus:outline-none focus:border-indigo-500"
+          />
         </div>
-        <button type="submit">Create Post</button>
+        <button
+          type="submit"
+          class="w-full bg-indigo-500 text-white px-3 py-2 text-sm rounded-lg hover:bg-indigo-600 font-medium"
+        >
+          Create Post
+        </button>
       </form>
     `;
   }
