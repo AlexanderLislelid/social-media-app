5d6eb830ce21c0978f39d737224bfa411b8fa9cc
AlexanderLislelid <alexlislelid@gmail.com>
2026-02-18 16:36:36 +0100

    Implement user profile data fetching in renderUser function

--- src/views/UserView.js
+++ src/views/UserView.js
@@ -1,5 +1,21 @@
+import { get } from "../api/apiClient.js";
+
 export function UserView() {
   return /* HTML */ `<h1>User Profile</h1>`;
 }
 
-export async function renderUser(username) {}
+export async function renderUser(username) {
+  try {
+    const response = await get(
+      `social/profiles/${username}?_following=true&_followers=true&_posts=true`,
+    );
+    if (!response) {
+      console.error("Failed to load user profile");
+      return;
+    }
+    const user = response.data;
+    console.log(user);
+  } catch (error) {
+    console.error("Failed to load user profile:", error);
+  }
+}
